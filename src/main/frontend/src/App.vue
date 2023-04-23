b4ee1d1dae12ee5e3dd9c2f5f10572f555b58c84
zajacszy <jzajacszymura@gmail.com>
2023-04-23 11:41:04 +0200

    komunikat założenia konta

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,8 +11,12 @@
       <button :class="signigUp ? 'button-outline' : '' " @click="signigUp=false">Logowanie</button>
       <button :class="!signigUp ? 'button-outline' :  '' " @click="signigUp=true">>Rejestracja</button>
 
+      <div v-if="message" class="alert">
+        {{message}}
+      </div>
+
       <LoginForm v-if="!signigUp" @login="(user) => logMeIn(user)"></LoginForm>
-      <LoginForm v-else @login="(user) => logMeIn(user)" button-label="Załóż konto"></LoginForm>
+      <LoginForm v-else @login="(user) => register(user)" button-label="Załóż konto"></LoginForm>
     </div>
   </div>
 </template>
@@ -22,11 +26,13 @@ import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
+import axios from "axios";
 
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
+      message: '',
       signigUp: false,
       authenticatedUsername: '',
     }
@@ -37,7 +43,20 @@ export default {
     },
     logMeOut() {
       this.authenticatedUsername = '';
+    },
+    register(user) {
+      axios.post('api/participants', user)
+          .then(response => {
+            // udało się
+            this.message = 'Udało się założyć konto'
+
+          })
+          .catch(response => {
+            // nie udało sie
+            this.message = 'Nie udało się założyć konta'
+          });
     }
+
   }
 }
 </script>
@@ -46,5 +65,14 @@ export default {
 #app {
   max-width: 1000px;
   margin: 0 auto;
+}
+
+.alert {
+  max-width: 300px;
+  margin: 20px;
+  padding: 20px;
+  color: green;
+  font-family: Arial;
+  border: 10px solid green;
 }
 </style>
